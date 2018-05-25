a7fdf2e3da34a2a98071ffa7943aae7cc1986c08
ksaric <kristijan.saric@iohk.io>
2018-05-25 12:28:40 +0200

    [CO-289] Add simple tests, pending tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,29 @@
+module Main where
+
+import           Universum
+
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+-- stack test log-classifier --fast --test-arguments "-m Zendesk"
+spec :: Spec
+spec = do
+    describe "Zendesk" $ do
+        processTicketSpec
+        processTicketsSpec
+
+processTicketSpec :: Spec
+processTicketSpec = do
+    describe "processTicket" $ modifyMaxSuccess (const 200) $ do
+        it "doesn't process ticket since the ticket cannot be found" $
+            pendingWith "Not implemented!"
+
+processTicketsSpec :: Spec
+processTicketsSpec =
+    describe "processTickets" $ do
+        it "doesn't process tickets" $ do
+            pending
