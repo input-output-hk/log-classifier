75a610f9a2226be23c8568be6c99306a28f02b1f
Rob Cohen <rob.cohen@iohk.io>
2018-07-18 08:47:48 -0400

    [TSD-80] - Removed old nix files, renamed release1 to shell, took excess comments out of stack.yaml

--- fetch-nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-let
-  spec = builtins.fromJSON (builtins.readFile ./nixpkgs-src.json);
-in
-  import ./fetchNixpkgs.nix spec
--- shell.nix
+++ shell.nix
@@ -1,46 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
 let
+  bootstrap = import <nixpkgs> { };
 
-  inherit (nixpkgs) pkgs;
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
 
-  f = { mkDerivation, aeson, array, attoparsec, base, bytestring
-      , containers, directory, generics-sop, hspec, http-conduit
-      , monad-control, mtl, optparse-applicative, QuickCheck, reflection
-      , regex-tdfa, resource-pool, sqlite-simple, stdenv, text, time
-      , transformers-base, universum, zip-archive
-      }:
-      mkDerivation {
-        pname = "log-classifier";
-        version = "1.1.0.0";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        libraryHaskellDepends = [
-          aeson array attoparsec base bytestring containers directory
-          generics-sop http-conduit monad-control mtl optparse-applicative
-          QuickCheck reflection regex-tdfa resource-pool sqlite-simple text
-          time transformers-base universum zip-archive
-        ];
-        executableHaskellDepends = [ base universum ];
-        testHaskellDepends = [
-          aeson array attoparsec base bytestring containers hspec
-          http-conduit mtl QuickCheck reflection regex-tdfa text universum
-          zip-archive
-        ];
-        homepage = "https://github.com/input-output-hk/log-classifier#readme";
-        description = "Log classifier for a Cardano node";
-        license = stdenv.lib.licenses.mit;
-      };
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
 
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
+  pkgs = import src { };
 
 in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+  pkgs.haskellPackages.callPackage ./default.nix { }
