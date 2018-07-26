ae9b0a3f573f035cdce06843339f242fe7db9f4a
ksaric <kristijan.saric@iohk.io>
2018-07-26 13:22:04 +0200

    [TSD-86] Create visual architecture of the project.

---
+++ src/Configuration.hs
@@ -0,0 +1,40 @@
+-- Here we need to export just the public info.
+module Configuration
+    ( defaultConfig
+    ) where
+
+import           Universum
+
+import           DataSource.DB
+import           DataSource.Http
+import           DataSource.Types
+import           HttpLayer (basicHTTPNetworkLayer)
+
+
+-- | The default configuration.
+defaultConfig :: Config
+defaultConfig = Config
+    { cfgAgentId                = 0
+    , cfgZendesk                = "https://iohk.zendesk.com"
+    , cfgToken                  = ""
+    , cfgEmail                  = "daedalus-bug-reports@iohk.io"
+    , cfgAssignTo               = 0
+    , cfgKnowledgebase          = []
+    , cfgNumOfLogsToAnalyze     = 5
+    , cfgIsCommentPublic        = False -- TODO(ks): For now, we need this in CLI.
+    , cfgDataLayer              = basicDataLayer
+    , cfgHTTPNetworkLayer       = basicHTTPNetworkLayer
+    , cfgIOLayer                = basicIOLayer
+    , cfgDBLayer                = connDBLayer
+    }
+
+-- | The @IO@ layer.
+basicIOLayer :: (MonadIO m, MonadReader Config m) => IOLayer m
+basicIOLayer = IOLayer
+    { iolAppendFile             = appendFile
+    , iolPrintText              = putTextLn -- iolPrintConsole
+    , iolReadFile               = readFile
+    , iolLogDebug               = putTextLn
+    , iolLogInfo                = putTextLn
+    }
+
