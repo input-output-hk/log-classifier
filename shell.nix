aac02c3099d9267395fa98b9937965741d07e9c3
Rob Cohen <rob.cohen@iohk.io>
2018-08-27 14:02:00 -0400

    [TSD-117] - Another shot

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskell.packages.ghc822.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          universum =
+            pkgs.haskell.lib.dontCheck
+              (haskellPackagesNew.callHackage "universum" "1.1.0" {});
+          log-classifier =
+            (haskellPackagesNew.callCabal2nix "log-classifier" ./. {});
+        };
+      };
+    };
+  };
+in
+  { pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
+  pkgs.haskell.lib.buildStackProject {
+    name = "log-classifier";
+    inherit ghc;
+    buildInputs = with pkgs; [ unzip zlib ];
+    LANG = "en_US.UTF-8";
+  }
