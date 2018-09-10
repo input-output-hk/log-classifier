91236c35d99dffc1500b6a2ca3c2784f8edcfe2e
Hiroto Shioi <shioihi@me.com>
2018-09-10 16:17:46 +0900

    [TSD-135] Update to version 1.4.5 (#60)
    * [TSD-135] Upgrade to 1.4.5
    
    * Switch public flag

--- src/DataSource/Types.hs
+++ src/DataSource/Types.hs
@@ -464,6 +464,7 @@ data TicketTag
     | AnalyzedByScriptV1_4_2  -- ^ Ticket has been analyzed by the version 1.4.2
     | AnalyzedByScriptV1_4_3  -- ^ Ticket has been analyzed by the version 1.4.3
     | AnalyzedByScriptV1_4_4  -- ^ Ticket has been analyzed by the version 1.4.4
+    | AnalyzedByScriptV1_4_5  -- ^ Ticket has been analyzed by the version 1.4.5
     | ToBeAnalyzed            -- ^ Ticket needs to be analyzed
     | NoKnownIssue            -- ^ Ticket had no known issue
     | NoLogAttached           -- ^ Log file not attached
@@ -974,6 +975,7 @@ renderTicketStatus AnalyzedByScriptV1_4_1 = "analyzed-by-script-v1.4.1"
 renderTicketStatus AnalyzedByScriptV1_4_2 = "analyzed-by-script-v1.4.2"
 renderTicketStatus AnalyzedByScriptV1_4_3 = "analyzed-by-script-v1.4.3"
 renderTicketStatus AnalyzedByScriptV1_4_4 = "analyzed-by-script-v1.4.4"
+renderTicketStatus AnalyzedByScriptV1_4_5 = "analyzed-by-script-v1.4.5"
 renderTicketStatus ToBeAnalyzed           = "to_be_analysed" -- https://iohk.zendesk.com/agent/admin/tags
 renderTicketStatus NoKnownIssue           = "no-known-issues"
 renderTicketStatus NoLogAttached          = "no-log-files"
