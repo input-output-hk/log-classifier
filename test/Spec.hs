b3b4f6b641f60e649c75c160aeb99577824f1971
Rob Cohen <rob.cohen@iohk.io>
2018-09-14 09:33:57 -0400

    [TSD-140] - Versioning change in Spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -596,7 +596,7 @@ createResponseTicketSpec =
                     responseTags        = getTicketTags $ tTag responseTicket
                 -- in summary, the response tags have the debuggers `analyzed-by-script-version` tag AND
                 -- they remove the `to_be_analysed` tag AND they are unique.
-                in (filter (/= renderTicketStatus ToBeAnalyzed) . nub $ renderTicketStatus AnalyzedByScriptV1_4_6 : mergedTags) === responseTags
+                in (filter (/= renderTicketStatus ToBeAnalyzed) . nub $ renderTicketStatus AnalyzedByScriptV1_5_0 : mergedTags) === responseTags
 
 
 exportZendeskDataToLocalDBSpec :: Spec
