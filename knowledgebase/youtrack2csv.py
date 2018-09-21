5ee6415622f415f162c153eabf05a94b34be4051
Rob Cohen <rob.cohen@iohk.io>
2018-09-21 11:01:28 -0400

    [TSD-142] - Remove youtrack python script

--- knowledgebase/youtrack2csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import csv
-
-df = pd.read_csv('issues.csv', dtype={'Zendesk Debugger FAQ Number': str})
-df.set_index('Issue Id')
-df = df[['Zendesk Debugger Identifier Text', 'Zendesk-Debugger-Error-Code', 'Summary', 'Issue Id', 'Zendesk Debugger FAQ Number']]
-df = df[~df['Zendesk Debugger Identifier Text'].isin(['No zendesk debugger identifier text'])]
-df = df[~df['Zendesk-Debugger-Error-Code'].isin(['No zendesk-debugger-error-code'])]
-df.to_csv('tmpknowledge.csv', index = False, header = False, quotechar='"', quoting=csv.QUOTE_ALL)
