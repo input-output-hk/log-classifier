b8322ad25d2bdfaf8c31178982722f7de3614592
Samuel Leathers <samuel.leathers@iohk.io>
2018-11-15 20:50:48 -0500

    add stub log-classifier-web test
    * rename web -> log-classifier-web
    * update to use GHC 8.4.3
    * fix release.nix

--- nix/overlays/required.nix
+++ nix/overlays/required.nix
@@ -5,12 +5,13 @@ with import ../../lib.nix;
 with pkgs.haskell.lib;
 
 self: super: rec {
-        universum           = pkgs.haskell.lib.dontCheck (self.callHackage "universum" "1.1.0" {});
+        #universum           = pkgs.haskell.lib.dontCheck (self.callHackage "universum" "1.1.0" {});
         # because of http://hackage.haskell.org/package/concurrent-output
         concurrent-output   = pkgs.haskell.lib.dontCheck (self.callPackage ../../concurrent-output.nix {});
         stm                 = pkgs.haskell.lib.dontCheck (self.callHackage "stm" "2.4.5.1" {});
+        conduit-extra       = pkgs.haskell.lib.dontCheck super.conduit-extra;
 
         # LIB
-        log-classifier      = self.callCabal2nix "log-classifier" ../../classifier { };
-        log-classifier-web  = self.callCabal2nix "web" ../../web { };
+        log-classifier      = self.callCabal2nix "log-classifier" ../../log-classifier { };
+        log-classifier-web  = self.callCabal2nix "log-classifier-web" ../../log-classifier-web { };
 }
